84f55f0c6ba805e05f51299257b9a080f6596026
Nayef06 <nayef.jaber10@gmail.com>
2025-11-03 19:12:43 -0800

    feat: add user interface and implement user management with session and passport integration

--- Notes/Express/src/handlers/users.ts
+++ Notes/Express/src/handlers/users.ts
@@ -1,8 +1,12 @@
-import { Request, Response } from "express";
+import { Request, Response } from "express-serve-static-core";
 import { CreateUserDto } from "../dtos/CreateUser.dto";
 import { CreateUserQueryParams } from "../types/query-params";
+import { User } from "../types/response";
+import session from "express-session";
+import passport from "passport";
 
 export function getUsers(request: Request, response: Response) {
+    request.customField
     response.send([])
 }
 
@@ -10,9 +14,18 @@ export function getUserById(request: Request, response: Response) {
     response.send({})
 }
 
-export function createUser(request: Request<{
-        id: string
-    },
-    {}, CreateUserDto, CreateUserQueryParams>, response: Response) {
-        request.query.loginAfterCreate
-    }
+export function createUser(
+    request: Request<{id: string},{}, CreateUserDto, CreateUserQueryParams>, 
+    response: Response<User>
+)   {
+        //request.query.loginAfterCreate
+        //response.status(201).send({}) -> errors becasue of <User>
+
+        //request.user
+
+        response.status(201).send({
+            id: 1,
+            username: 'Luffy',
+            email: "test@domain.com"
+        })
+}
