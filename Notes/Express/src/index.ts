c5e81fcfb8ad30f6e82475a6e6c184009e69d090
Nayef06 <nayef.jaber10@gmail.com>
2025-11-05 20:31:22 -0800

    feat: implement user management with Express and add end-to-end tests

--- Notes/Express/src/index.ts
+++ Notes/Express/src/index.ts
@@ -1,15 +1,16 @@
 import express, { NextFunction, Request, Response } from 'express';
 import usersRouter from './routes/users'
+import { createApp } from './createApp';
+// const app = express();
 
-const app = express();
-
-app.use('/api/users', usersRouter)
+// app.use('/api/users', usersRouter)
+const app = createApp()
 
 const PORT = 3000;
 
-app.get('/api/example', (request: Request, response: Response, next: NextFunction)=>{
-    response.send([])
-})
+// app.get('/api/example', (request: Request, response: Response, next: NextFunction)=>{
+//     response.send([])
+// })
 
 app.listen(PORT, ()=>{
     console.log(`Running on Port ${PORT}`);
